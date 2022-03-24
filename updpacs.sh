8ac05d3f76d268200df5a5402741e31d27695cef
Macklemurr <makacervo@gmail.com>
2022-03-23 21:32:29 -0400

    added while loops

--- updpacs.sh
+++ updpacs.sh
@@ -6,13 +6,21 @@ pac=$(/usr/bin/pacman -Qu | wc -l)
 # Needs to Sync the repostiory before querying for packages that need to be updated
 sudo pacman -Syy
 
+while [ $pac -ge 1 ]; 
+do
 echo "There are $pac Packages that need to be updated! Would you like to update now? y/n: " 
 read input 
 
-if [ $input ==  y ]; then
-	sudo pacman -Su --noconfirm 
+	if [ $input ==  y ]; then
+		sudo pacman -Su --noconfirm 
 
-elif [ $input == n ]; then
+	elif [ $input == n ]; then
+		echo "Exiting Package Updater!"
+		exit 1
+	fi
+done
+while [ $pac == 0 ];
+do
+	echo "No Packages need Updating! Exiting now."
 	exit 1
-fi
-
+done
