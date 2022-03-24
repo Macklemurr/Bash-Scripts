ed76311998169dbddf5c3ca32535d58c7560ae08
Macklemurr <makacervo@gmail.com>
2022-03-23 21:57:53 -0400

    Redundant pac sync line

--- updpacs.sh
+++ updpacs.sh
@@ -3,16 +3,13 @@ clear
 # Queries available packages and lists the number of lines
 pac=$(/usr/bin/pacman -Qu | wc -l)
 
-# Needs to Sync the repostiory before querying for packages that need to be updated
-sudo pacman -Syy
-
 while [ $pac -ge 1 ]; 
 do
 echo "There are $pac Packages that need to be updated! Would you like to update now? y/n: " 
 read input 
 
 	if [ $input ==  y ]; then
-		sudo pacman -Su --noconfirm 
+		sudo pacman -Syyu --noconfirm 
 
 	elif [ $input == n ]; then
 		echo "Exiting Package Updater!"
