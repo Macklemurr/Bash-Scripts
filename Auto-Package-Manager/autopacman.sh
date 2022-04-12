2caf86483f31b767ce557e038c2e27ca3ab7ebb2
Macklemurr <makacervo@gmail.com>
2022-04-12 11:26:26 -0400

    added auto add feature for the file

--- Auto-Package-Manager/autopacman.sh
+++ Auto-Package-Manager/autopacman.sh
@@ -4,7 +4,8 @@
 #-For the best use of this script make a symbolic----#
 #-to your $PATH, for more info read the README.md----#
 #----------------------------------------------------#
-source ./foo.sh
+file="find $HOME 2> /dev/null | grep autopm.sh"
+source $file
 clear
 echo -e "This script was made by Macklemurr on Github! :3\n"
 echo -e "Please read the README.md for the best usage of this script\n"
