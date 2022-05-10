8e6c79452df726605bc73ada3322a8878fd8ef40
Macklemurr <makacervo@gmail.com>
2022-05-10 18:09:36 -0400

    simplifying the code

--- adduser.sh
+++ adduser.sh
@@ -18,58 +18,52 @@ while [ $(($#)) -eq 0 ] || [ $(($#)) -ge 6 ]; do
 done
 while [ "$(($#))" != 0 ] && [ "$(($#))" != 5 ]; do
 	case $1 in
-	-a ) 
+	-U ) # User 
 		sudo_check ; 
-			if [ "$3" = --no-home ] || [ "$3" = -nh ]; then
-				sudo useradd -M "$2"
-				echo "User $2 was created with no home directory"
+			if [ "$3" = --no-home ] || [ "$3" = -nh ];  then
+				sudo useradd -M "$2" ; 
+				echo "User $2 was created with no home directory" ; 
 				exit 0
-			fi
-		sudo useradd -m "$2" ;
-		echo "$2 was created with a home directory" ;
-	        exit 0 	
-		;;
-	-h ) 
-		echo "Usage: adduser <option> <username> <argument> <modifier>" ;
+			elif [ "$3" = -d ] || [ "$3" = --delete ]; then 
+				sudo userdel "$2" ; 
+				echo "$2 has been deleted" ; 
+				exit 0 
+			elif [ "$3" = -dh ] || [ "$3" = --delete-home ] ; then
+				sudo userdel -r "$2" ; 
+				echo "$2 has been deleted with their home directory" ; 
+				exit 0
+			fi	
+		sudo useradd -m "$2" ; echo "$2 was created with a home directory" ; exit 0 ;; 
+	-h ) # Help  
+		echo "Usage: adduser <option> <argument> <argument> <modifier>" ;
 		echo "---------------------------" ;
 		echo "OPTIONS:" ;
-		echo "-a <username>: adds a user, by default a home directory is made" ;
-		echo "-aG <username> <group>: Creates a user to a group" ;
-		echo "-d <username>: deletes a user, by default the home directory is not deleted" ;
+		echo "-U <username>: adds a user, by default a home directory is made" ;
+		echo "-G <username> <group>: Creates a user to a group" ;
 		echo "-p <username>: changes a user password" ;
 		echo "-id or --change-id <user id> <username>: changes an existing user's UID" ;
 		echo "---------------------------" ;
 		echo "Modifiers:" ;
-		echo "-a: -nh --no-home, creates a user with no home directory" ;
-		echo "-d: -h --home, deletes a user with their home directory" ;
+		echo "----User-----" ;
+		echo "-U: -nh, --no-home: creates a user with no home directory" ;
+		echo "-U: -dh, --delete-home: deletes a user with their home directory" ;
+		echo "-U: -d, --delete: deletes a user but retains their home directory"
+		echo "----Group----" ;
+		echo "-G: -e, --existing, adds an existing user to a group" ;
+		echo "-G: -d, --delete, de" ;
 		exit 0 
 		;;
-	-aG )	
-		sudo_check ;
-		sudo useradd -m "$2" -G "$3" ;
-		echo "$2 is added to group $3" ;
-		exit 0
-		;;
-	-id )
-		sudo_check ;
-		sudo 
-		;;
-	-d )  
-		sudo_check ;
-			if [ "$3" = --home ] || [ "$3" = -h ]; then
-				sudo userdel -r "$2"
-				echo "$2 has been deleted with their home directory"
-				exit 0
+	-G )	# Group 
+		sudo_check ; 
+			if  [ "$4" = -e ] ; then
+				sudo groupmod -a $2 -G $3 ;
+			       	echo "$2 is added to group $3" ; exit 0		
+			elif  [ "$4" = -d ] ; then
+				sudo 
 			fi
-		sudo userdel "$2" ;
-		echo "$2 has been deleted" ; 
-		exit 0
-		;;
+		sudo useradd -m "$2" -G "$3" ;
+		echo "User $2 is created with the group  $3" ; exit 0 ;;
 	-p )  
-		sudo_check ;
-		sudo passwd "$2" ;
-		echo "$2's password has changed" ;
-		exit 0 
-		;;
+		sudo_check ; sudo passwd "$2" ; echo "$2's password has changed" ; exit 0 ;;
 	esac
 done
